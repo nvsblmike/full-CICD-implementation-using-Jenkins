778601ed0672ad1c55186880518edc389cb8b3cc
nvsblmike <soundboylesh77@gmail.com>
2025-04-25 15:14:38 +0100

    ipdates

--- main.tf
+++ main.tf
@@ -320,7 +320,7 @@ resource "aws_instance" "jenkins" {
 
 resource "aws_instance" "sonarqube" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = module.vpc.public_subnets[0]
   vpc_security_group_ids = [aws_security_group.sonarqube_sg.id]
   key_name               = aws_key_pair.generated_key.key_name
